b5c84bee6a989fa3178740e0091199ef348e264d
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-06-13 18:15:19 -0500

    worked level order method and level range method

--- lib/compairable.rb
+++ lib/compairable.rb
@@ -12,6 +12,37 @@ module Comparable
       end
   end
 
+  def find(interger)
+    @root.compact.each { |node|  
+    if node.data == interger
+    puts "Node at index: |#{node.index}|\n---Value |#{node.data}|"
+    end}
+  end
+
+# find levels order by ranges of power of two
+
+  def level_range(level_goal)
+    @lower = 0
+    @upper = 0
+    @current_level = 0
+    while @current_level != level_goal
+      @lower = @upper + 1
+      @upper = @lower * 2
+      @current_level += 1
+    end
+    @range = (@lower..@upper)
+    @range
+  end
+
+  def level_order(level)
+    @levelrange = level_range(level)
+    @nodes_in_range = @root.slice(@levelrange)
+    @nodes_array = []
+    @nodes_in_range.compact.each { |node| 
+     @nodes_array.push("Node at index: |#{node.index}|\n---Value |#{node.data}|\n---Level |#{level}|")
+    }
+    puts @nodes_array
+  end
 
 end
 
